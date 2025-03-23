0080f9a6a62c166d64a64ec306a47ab90f5ae12a
heejin131 <journyjean131@gmail.com>
2025-03-23 22:48:22 +0900

    10장 완료

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,17 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      // 클라이언트에서 bcrypt를 사용하지 않도록 설정
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        bcrypt: false,
-      };
-    }
-    return config;
-  },
+  // webpack(config, { isServer }) {
+  //   if (!isServer) {
+  //     // 클라이언트에서 bcrypt를 사용하지 않도록 설정
+  //     config.resolve.fallback = {
+  //       ...config.resolve.fallback,
+  //       bcrypt: false,
+  //     };
+  //   }
+  //   return config;
+  // },
+  experimental: {ppr: 'incremental'}
 };
 
 export default nextConfig;
